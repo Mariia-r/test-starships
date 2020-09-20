6a1afffd3706eef0ab49de0cb93834cc6a726075
mariia_rv <mariya-r1@ukr.net>
2020-09-20 16:36:31 +0300

    save image url to localStorage instead of array

--- src/store/saga.js
+++ src/store/saga.js
@@ -2,11 +2,17 @@ import { getStarshipsData, getStarshipsImg } from "../api/api";
 import { all, call, takeLatest, put } from "redux-saga/effects";
 import {GET_STARSHIPS, acSetStarships} from "../store/starships-reducer";
 
-function* getArrayStarShipsWithImg(starshipsData) {
+function* getStarshipsWithImg(starshipsData) {
     try {
         let starshipsWithImg = [];
         yield all(starshipsData.map(function* (item, index) {
-            let starshipImg = yield call(getStarshipsImg, item.name);
+            let starshipImg;
+            if (localStorage.getItem(`${item.name}-img`) !== (null || "undefined")) {
+                starshipImg = localStorage.getItem(`${item.name}-img`);
+            } else {
+                let starshipImg = yield call(getStarshipsImg, item.name);
+                localStorage.setItem(`${item.name}-img`, starshipImg);
+            }
             starshipsWithImg.push({name: item.name, img: starshipImg});
         }));
         return starshipsWithImg;
@@ -17,14 +23,8 @@ function* getArrayStarShipsWithImg(starshipsData) {
 
 export function* getStarships(action) {
     try {
-        let starshipsWithImg;
-        if (localStorage.getItem('starshipsWithImg') !== null) {
-            starshipsWithImg = JSON.parse(localStorage.getItem("starshipsWithImg"));
-        } else {
-            let starshipsData = yield call(getStarshipsData);
-            starshipsWithImg = yield call(getArrayStarShipsWithImg, starshipsData);
-            localStorage.setItem("starshipsWithImg", JSON.stringify(starshipsWithImg));
-        }
+        let starshipsData = yield call(getStarshipsData);
+        let starshipsWithImg = yield call(getStarshipsWithImg, starshipsData);
         yield put(acSetStarships(starshipsWithImg));
     } catch(e) {
         console.log(e);
